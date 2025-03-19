aac75abdf3968096d0bc5ef28eaa49f195a27012
July230 <A01352823@tec.mx>
2025-03-19 17:36:01 -0600

    primera cancion

--- inicio.py
+++ inicio.py
@@ -10,6 +10,7 @@ from music21 import note, stream
 # G - Sol
 miNota = note.Note("C")
 
+# funcion que imprime una nota que dura 3 tiempos de negra
 def imprimirNota():
 
     # Abrir el editor de partituras con la nota
@@ -23,18 +24,44 @@ def imprimirNota():
     print(miNota.pitch.spanish)
     print(miNota.duration.quarterLength)
 
+    # quarterLength = 1 tiempo de negra (default)
     miNota.duration.quarterLength = 3
     print(miNota.duration.quarterLength)
 
-do = note.Note('C')
-re = note.Note('D')
-mi = note.Note('E')
+# funcion que imprimer las notas do re y mi siendo mi una blanca
+def do_re_mi(unPentagrama):
+    do = note.Note('C')
+    pentagrama.append(do)
+
+    re = note.Note('D')
+    pentagrama.append(re)
+
+    mi = note.Note('E')
+    mi.duration.quarterLength = 2
+    pentagrama.append(mi)
+
+# funcion que imprimer las notas re y mi
+def re_mi(un_pentagrama):
+    re = note.Note('D')
+    un_pentagrama.append(re)
+
+    mi = note.Note('E')
+    un_pentagrama.append(mi)
+
+# funcion que imprimer la nota do
+def do(un_pentagrama):
+    do = note.Note('C')
+    do.duration.quarterLength = 2
+    un_pentagrama.append(do)
 
 # Crear el pentagrama que va a contener las notas de la partitura
 pentagrama = stream.Stream()
-pentagrama.append(do)
-pentagrama.append(re)
-pentagrama.append(mi)
+do_re_mi(pentagrama)
+do_re_mi(pentagrama)
+re_mi(pentagrama)
+re_mi(pentagrama)
+do(pentagrama)
+do(pentagrama)
 pentagrama.show()
 
 # Abrir el reproductor de midi's con un archivo midi con la nota Do
