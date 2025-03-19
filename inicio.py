379ff24cb98852e6b2ec54c8520ce33b1f2726e8
July230 <A01352823@tec.mx>
2025-03-19 17:20:40 -0600

    Crear un pentagrama

--- inicio.py
+++ inicio.py
@@ -1,4 +1,4 @@
-from music21 import note
+from music21 import note, stream
 
 # Nota
 # A - La
@@ -10,19 +10,32 @@ from music21 import note
 # G - Sol
 miNota = note.Note("C")
 
-# Abrir el editor de partituras con la nota
-miNota.show()
+def imprimirNota():
+
+    # Abrir el editor de partituras con la nota
+    miNota.show()
+
+    print(miNota)
+    print(miNota.name)
+    print(miNota.octave)
+    print(miNota.pitch)
+    print(miNota.pitch.frequency)
+    print(miNota.pitch.spanish)
+    print(miNota.duration.quarterLength)
+
+    miNota.duration.quarterLength = 3
+    print(miNota.duration.quarterLength)
+
+do = note.Note('C')
+re = note.Note('D')
+mi = note.Note('E')
+
+# Crear el pentagrama que va a contener las notas de la partitura
+pentagrama = stream.Stream()
+pentagrama.append(do)
+pentagrama.append(re)
+pentagrama.append(mi)
+pentagrama.show()
 
 # Abrir el reproductor de midi's con un archivo midi con la nota Do
-miNota.show('midi')
-
-print(miNota)
-print(miNota.name)
-print(miNota.octave)
-print(miNota.pitch)
-print(miNota.pitch.frequency)
-print(miNota.pitch.spanish)
-print(miNota.duration.quarterLength)
-miNota.duration.quarterLength = 3
-print(miNota.duration.quarterLength)
-miNota.show()
+# miNota.show('midi')
