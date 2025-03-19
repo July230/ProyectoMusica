7c4160491b40d5d46fe27cf0431634d48d9cda2f
July230 <A01352823@tec.mx>
2025-03-19 17:12:44 -0600

    Una nota que se abre en el editor de partituras y con atributos sobre las notas

--- inicio.py
+++ inicio.py
@@ -1,9 +1,24 @@
 from music21 import note
 
-# Nota Do
+# Nota
+# A - La
+# B - Si
+# C - Do
+# D - Re
+# E - Mi
+# F - Fa
+# G - Sol
 miNota = note.Note("C")
 
-# Abrir el editor
+# Abrir el editor de partituras con la nota
 miNota.show()
 
+# Abrir el reproductor de midi's con un archivo midi con la nota Do
 miNota.show('midi')
+
+print(miNota)
+print(miNota.name)
+print(miNota.octave)
+print(miNota.pitch)
+print(miNota.pitch.frequency)
+print(miNota.pitch.spanish)
