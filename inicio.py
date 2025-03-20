5b6afa6114ae7f50d35e9d26b59f0e282bba5d3c
July230 <A01352823@tec.mx>
2025-03-19 19:56:05 -0600

    Fragmento de pieza gwyn lord of cinder

--- inicio.py
+++ inicio.py
@@ -131,6 +131,9 @@ def claveDeSol(unPentagrama):
 def guitarra(unPentagrama):
     unPentagrama.insert(0, instrument.Guitar()) # (intervalo del compas, instrumento)
 
+def piano(unPentagrama):
+    unPentagrama.insert(0, instrument.Piano())
+
 def tiempoTresCuartos(unPentagrama):
     tiempoTresCuartos = meter.TimeSignature("3/4")
     unPentagrama.append(tiempoTresCuartos)
@@ -153,10 +156,20 @@ def doYFa(unPentagrama):
     doYFA.duration.quarterLength = 0.5
     unPentagrama.append(doYFA)
 
+def reYSol(unPentagrama):
+    reYSol = chord.Chord(["D4", "G4"])
+    reYSol.duration.quarterLength = 0.5
+    unPentagrama.append(reYSol)
+
 def laDoYFa(unPentagrama):
     laDoYFa = chord.Chord(["A2", "C4", "F4"])
     laDoYFa.duration.quarterLength = 0.5
     unPentagrama.append(laDoYFa)
+
+def laReYSol(unPentagrama):
+    laReYSol = chord.Chord(["A2", "D4", "G4"])
+    laReYSol.duration.quarterLength = 0.5
+    unPentagrama.append(laReYSol)
 
 def la3DoYFa(unPentagrama):
     la3DoYFa = chord.Chord(["A3", "C4", "F4"])
@@ -168,10 +181,20 @@ def siYMi(unPentagrama):
     siYMi.duration.quarterLength = 0.5
     unPentagrama.append(siYMi)
 
+def doYMi(unPentagrama):
+    doYMi = chord.Chord(["E3", "C4"])
+    doYMi.duration.quarterLength = 0.5
+    unPentagrama.append(doYMi)
+
 def si(unPentagrama):
     si3 = note.Note('B3')
     si3.duration.quarterLength = 0.5
     unPentagrama.append(si3)
+
+def do(unPentagrama):
+    do4 = note.Note('C4')
+    do4.duration.quarterLength = 0.5
+    unPentagrama.append(do4)
 
 def miOctavado(unPentagrama):
     miOctavado = chord.Chord(["E5", "E6"])
@@ -181,11 +204,24 @@ def reOctavado(unPentagrama):
     reOctavado = chord.Chord(["D5", "D6"])
     unPentagrama.append(reOctavado)
 
-def laOctavado(unPentagrama):
-    laOctavado = chord.Chord(["A4", "A5"])
-    laOctavado.duration.quarterLength = 3
+def laOctavadoTresTiempos(unPentagrama):
+    laOctavadoTresTiempos = chord.Chord(["A4", "A5"])
+    laOctavadoTresTiempos.duration.quarterLength = 3
+    unPentagrama.append(laOctavadoTresTiempos)
+
+def solOctavadoDosTiempos(unPentagrama):
+    laOctavado = chord.Chord(["G5", "G6"])
+    laOctavado.duration.quarterLength = 2
     unPentagrama.append(laOctavado)
 
+def faOctavado(unPentagrama):
+    faOctavado = chord.Chord(["F5", "F6"])
+    unPentagrama.append(faOctavado)
+
+def miOctavadoTresTiempos(unPentagrama):
+    miOctavadoTresTiempos = chord.Chord(["E5", "E6"])
+    miOctavadoTresTiempos.duration.quarterLength = 3
+    unPentagrama.append(miOctavadoTresTiempos)
 
 
 # Crear la partitura
@@ -196,7 +232,7 @@ partitura = stream.Score()
 pentagrama1 = stream.Part()
 claveDeFa(pentagrama1)
 tiempoTresCuartos(pentagrama1)
-guitarra(pentagrama1)
+piano(pentagrama1)
 laMi(pentagrama1)
 siDoYFa(pentagrama1)
 si(pentagrama1)
@@ -208,7 +244,7 @@ la3DoYFa(pentagrama1)
 si(pentagrama1)
 doYFa(pentagrama1)
 si(pentagrama1)
-for i in range(4):
+for i in range(2):
     laDoYFa(pentagrama1)
     siYMi(pentagrama1)
     la3DoYFa(pentagrama1)
@@ -216,15 +252,30 @@ for i in range(4):
     doYFa(pentagrama1)
     si(pentagrama1)
 
+for i in range(4):
+    laReYSol(pentagrama1)
+    doYMi(pentagrama1)
+    reYSol(pentagrama1)
+    do(pentagrama1)
+    reYSol(pentagrama1)
+    do(pentagrama1)
+
 # Crear segundo pentagrama
 pentagrama2 = stream.Part()
 claveDeSol(pentagrama2)
 tiempoTresCuartos(pentagrama2)
-guitarra(pentagrama2)
+piano(pentagrama2)
 silencioDeNegra(pentagrama2)
 miOctavado(pentagrama2)
 reOctavado(pentagrama2)
-laOctavado(pentagrama2)
+laOctavadoTresTiempos(pentagrama2)
+silencioDeNegra(pentagrama2)
+miOctavado(pentagrama2)
+reOctavado(pentagrama2)
+solOctavadoDosTiempos(pentagrama2)
+faOctavado(pentagrama2)
+miOctavadoTresTiempos(pentagrama2)
+
 
 partitura.append(pentagrama2)
 partitura.append(pentagrama1)
