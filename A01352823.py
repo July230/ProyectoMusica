ce9eedad8749ccc3265f74442061dd87824571ad
July230 <A01352823@tec.mx>
2025-03-19 20:11:35 -0600

    Quitar funciones sin usar

--- A01352823.py
+++ A01352823.py
@@ -37,58 +37,6 @@ def definirTempo(unPentagrama):
     unPentagrama.append(tiempo)
     print(tiempo.text)
 
-# funcion que imprimer las notas do re y mi siendo mi una blanca
-def do_re_mi(unPentagrama):
-    do = note.Note('C')
-    pentagrama.append(do)
-
-    re = note.Note('D')
-    pentagrama.append(re)
-
-    mi = note.Note('E')
-    mi.duration.quarterLength = 2
-    pentagrama.append(mi)
-
-# funcion que imprimer las notas re y mi
-def re_mi(un_pentagrama):
-    re = note.Note('D')
-    un_pentagrama.append(re)
-
-    mi = note.Note('E')
-    un_pentagrama.append(mi)
-
-# funcion que imprimer la nota do
-def do(un_pentagrama):
-    do = note.Note('C')
-    do.duration.quarterLength = 2
-    un_pentagrama.append(do)
-
-def miSiMi(unPentagrama):
-    mi3 = note.Note('E3')
-    unPentagrama.append(mi3)
-    si3 = note.Note('B3')
-    unPentagrama.append(si3)
-    mi4 = note.Note('E4')
-    unPentagrama.append(mi4)
-
-def miSi3(unPentagrama):
-    mi3 = note.Note('E3')
-    unPentagrama.append(mi3)
-    si3 = note.Note('B3')
-    unPentagrama.append(si3)
-
-def miSi4(unPentagrama):
-    mi4 = note.Note('E4')
-    unPentagrama.append(mi4)
-    si4 = note.Note('B4')
-    unPentagrama.append(si4)
-
-def reDos5(unPentagrama):
-    re5 = note.Note('D5')
-    unPentagrama.append(re5)
-    doSostenido5 = note.Note('C5#')
-    unPentagrama.append(doSostenido5)
-
 def doMenor(unPentagrama):
     doMenor4 = chord.Chord(["C4", "E-4", "G4"])
     doMenor4.duration.quarterLength = 4
