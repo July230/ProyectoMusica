ae31b58476f7e1c6baf61d6fbb9871c8226a2351
July230 <A01352823@tec.mx>
2025-03-20 16:55:15 -0600

    Agregar funciones para notas en hollowknight

--- hollowknight.py
+++ hollowknight.py
@@ -6,7 +6,7 @@ from music21 import note, stream, chord, tempo, clef, meter, instrument
 
 def definirTempo(unPentagrama):
     # Crear un objeto de tempo (metronome mark)
-    tiempo = tempo.MetronomeMark(number=120)
+    tiempo = tempo.MetronomeMark(number=55)
     tiempo.number
     tiempo.referent
     tiempo.referent.type
@@ -18,6 +18,40 @@ def do4Sol4Lab4(unPentagrama):
     do4.duration.quarterLength = 0.5
     unPentagrama.append(do4)
 
+    sol4 = note.Note("G4")
+    sol4.duration.quarterLength = 0.5
+    unPentagrama.append(sol4)
+
+    lab4 = note.Note("A-4")
+    lab4.duration.quarterLength = 2
+    unPentagrama.append(lab4)
+
+def do4Fa4Sol4(unPentagrama):
+    do4 = note.Note("C")
+    do4.duration.quarterLength = 0.5
+    unPentagrama.append(do4)
+
+    Fa4 = note.Note("F4")
+    Fa4.duration.quarterLength = 0.5
+    unPentagrama.append(Fa4)
+
+    sol4 = note.Note("G4")
+    sol4.duration.quarterLength = 2
+    unPentagrama.append(sol4)
+
+def do4Fa4Sol4(unPentagrama):
+    do4 = note.Note("C")
+    do4.duration.quarterLength = 0.5
+    unPentagrama.append(do4)
+
+    mib4 = note.Note("E-4")
+    mib4.duration.quarterLength = 0.5
+    unPentagrama.append(mib4)
+
+    Fa4 = note.Note("F4")
+    Fa4.duration.quarterLength = 2
+    unPentagrama.append(Fa4)
+
 
 
 # Crear la partitura
@@ -25,9 +59,13 @@ partitura = stream.Score()
 
 # Crear primer pentagrama que va a contener la armonia
 pentagrama1 = stream.Part()
+definirTempo(pentagrama1)
 
 # Crear segundo pentagrama que va a contener la melodia
 pentagrama2 = stream.Part()
+definirTempo(pentagrama2)
+do4Sol4Lab4(pentagrama2)
+do4Fa4Sol4(pentagrama2)
 
 partitura.append(pentagrama2)
 partitura.append(pentagrama1)
