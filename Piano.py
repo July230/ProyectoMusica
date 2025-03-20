a6b617d26b03ec78312a475f43c985c8f440fa48
July230 <A01352823@tec.mx>
2025-03-20 17:30:28 -0600

    usar mixer para tocar escala de do a partir de archivos midi

--- Piano.py
+++ Piano.py
@@ -11,20 +11,43 @@ mixer.init()
 do = 'Notas/do.mid'
 re = 'Notas/re.mid'
 mi = 'Notas/mi.mid'
+fa = 'Notas/fa.mid'
+sol = 'Notas/sol.mid'
+la = 'Notas/la.mid'
+si = 'Notas/si.mid'
+do5 = 'Notas/do5.mid'
 
-notas = "asdasdasdsasdsadsada"
+notas = "asdfghjk"
 
-while True:
-    tecla = input()
-#for tecla in notas:
-    if tecla == 'a':
-        mixer.music.load(do)
-        mixer.music.play()
-    if tecla == 's':
-        mixer.music.load(re)
-        mixer.music.play()
-    if tecla == 'd':
-        mixer.music.load(mi)
-        mixer.music.play()
-    # time.sleep(0.8)
+#while True:
+    #tecla = input()
+
+def escalaDeDo(): 
+    for tecla in notas:
+        if tecla == 'a':
+            mixer.music.load(do)
+            mixer.music.play()
+        if tecla == 's':
+            mixer.music.load(re)
+            mixer.music.play()
+        if tecla == 'd':
+            mixer.music.load(mi)
+            mixer.music.play()
+        if tecla == 'f':
+            mixer.music.load(fa)
+            mixer.music.play()
+        if tecla == 'g':
+            mixer.music.load(sol)
+            mixer.music.play()
+        if tecla == 'h':
+            mixer.music.load(la)
+            mixer.music.play()
+        if tecla == 'j':
+            mixer.music.load(si)
+            mixer.music.play()
+        if tecla == 'k':
+            mixer.music.load(do5)
+            mixer.music.play()
+        time.sleep(0.5)
     
+escalaDeDo()
